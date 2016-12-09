44afcaac9c4387f763db37c3b4f20d79c619248c
Alejandro Espinosa <alex.espinosadelosmonteros@gmail.com>
2016-12-09 10:38:05 -0600

    Added sockect io Connect and Disconnect events

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
-const path = require('path')
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 // to not use server/../public
-const publicPath = path.join(__dirname, '../public')
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app)
+// to get back socket web server
+var io = socketIO(server);
 
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
+io.on('connection', (socket) => {
+  console.log('New user is connected');
 
+
+  socket.on('disconnect', () => {
+    console.log("User Disconnected");
+  })
 })
 
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Chat app is running on PORT ${port}`);
-})
+});
